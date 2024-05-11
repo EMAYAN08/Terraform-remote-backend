a47ab63ff96e9c741ebe0f61b7999469f9c971f1
Emayan Vadivel <emayanramalingam@gmail.com>
2024-05-11 19:23:25 -0300

    Adding Project files

---
+++ 04-ec2-static-backend/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-emayan"
+    region         = "us-west-2"
+    dynamodb_table = "terraform_state_lock"
+    key            = "dev/ec2.tfstate"
+    encrypt        = true
+  }
+}
+
+resource "aws_instance" "terraform-state-test" {
+  ami           = "ami-0cf2b4e024cdb6960"
+  instance_type = "t2.micro"
+  tags = {
+     Name = "test-instance"
+  }
+}
